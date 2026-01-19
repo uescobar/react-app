4c409ba452b6af3eff666e6e2c58927aca121ac1
uescobar <uescobar.360@gmail.com>
2026-01-19 17:39:20 -0600

    state vs props

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import Card, { CardBody } from "./components/Card";
 import List from "./components/List";
 // Todas la aplicaciones en React necesitan una función, por convención se llama App
 function App() {
+  const handleSelect = (elemento: string) => {
+    console.log("Elemento seleccionado en App:", elemento);
+  };
+  const handleSelect2 = (elemento: string) => {
+    console.log("Mostrando el elemento seleccionado:", elemento);
+  };
   const list = ["Goku", "Tanjiro", "Eren"];
   return (
     <Card>
       <CardBody title="Hola Mundo" text="Este es el texto" />
-      <List data={list} />
+      <List data={list} onSelect={handleSelect} />
+      <List data={list} onSelect={handleSelect2} />
     </Card>
   );
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,18 +1,25 @@
-import type { MouseEvent } from "react";
+import { useState, type MouseEvent } from "react";
 
 type Props = {
   data: string[];
+  onSelect?: (elemento: string) => void;
 };
 
-function List({ data }: Props) {
-  const handleClick = (e: MouseEvent) => {
-    console.log("click");
+function List({ data, onSelect }: Props) {
+  const [index, setIndex] = useState(1);
+  const handleClick = (i: number, elemento: string) => {
+    setIndex(i);
+    onSelect?.(elemento);
   };
 
   return (
     <ul className="list-group">
-      {data.map((elemento) => (
-        <li onClick={handleClick} key={elemento} className="list-group-item">
+      {data.map((elemento, i) => (
+        <li
+          onClick={() => handleClick(i, elemento)}
+          key={elemento}
+          className={`list-group-item ${index == i ? "active" : ""}`}
+        >
           {elemento}
         </li>
       ))}
