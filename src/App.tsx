d32aeb64056338fcab430bf3c5f8a8629a4a2e24
uescobar <uescobar.360@gmail.com>
2026-02-10 16:46:14 -0600

    Primera forma de crear el formulario}

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,9 @@
-import { useState } from "react";
-import ProductDashboard from "./components/ProductDashboard";
-import ProductList from "./components/ProductList";
-import Button from "./components/Button";
+import Form from "./components/Form";
 
 function App() {
-  const [products, setProducts] = useState([
-    {
-      id: Math.random().toString(),
-      name: "iPhone",
-    },
-  ]);
-
-  const comienzo = () => {
-    let product = { id: Math.random().toString(), name: "Surfboard" };
-    setProducts([product, ...products]);
-  };
-
-  const final = () => {
-    let product = { id: Math.random().toString(), name: "Guitarra Eléctrica" };
-    setProducts([...products, product]);
-  };
-
-  const eliminar = () => {
-    setProducts(products.slice(0, -1));
-  };
-
-  const limpiar = () => {
-    setProducts([]);
-  };
-
   return (
-    <div>
-      <Button onClick={comienzo}>Comienzo</Button>
-      <Button onClick={final}>Final</Button>
-      <Button onClick={eliminar}>Eliminar</Button>
-      <Button onClick={limpiar}>Limpiar</Button>
-      <ProductList products={products} />
+    <div className="container">
+      <Form />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
-//import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
