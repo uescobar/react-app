01a1b37bd1a2e1c277c613a23dab9ed113cd72ce
uescobar <uescobar.360@gmail.com>
2026-02-10 16:59:01 -0600

    Formulario Nombre Apellido

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,47 +1,31 @@
-import React from "react";
+import React, { type FormEvent } from "react";
 
 type Props = {};
 
 function Form({}: Props) {
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("Enviando...");
+  };
   return (
-    <form>
+    // emet
+    // form>div.mb-3*2>label.form-label+input#name.form-control
+    // button.btn.btn-primary --- emet
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
+        <label htmlFor="name" className="form-label">
+          Nombre
         </label>
-        <input
-          type="email"
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-        />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
-        </div>
+        <input type="text" id="name" className="form-control" />
       </div>
       <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-          Password
+        <label htmlFor="lastname" className="form-label">
+          Apellido
         </label>
-        <input
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-        />
+        <input type="text" id="lastname" className="form-control" />
       </div>
-      <div className="mb-3 form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="exampleCheck1"
-        />
-        <label className="form-check-label" htmlFor="exampleCheck1">
-          Check me out
-        </label>
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
+
+      <button className="btn btn-primary">Enviar</button>
     </form>
   );
 }
