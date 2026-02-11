df6a9197cfb084c2be647a3f56022cc128469d8e
uescobar <uescobar.360@gmail.com>
2026-02-11 17:40:58 -0600

    formulario react hook form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,41 +1,49 @@
-import React, { useRef, useState, type FormEvent } from "react";
+import { useForm } from "react-hook-form";
 
-type Props = {};
+type Form = {
+  name: string;
+  lastname: string;
+};
 
-function Form({}: Props) {
-  const [user, setUser] = useState({
-    name: "",
-    lastname: "",
-  });
+function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Form>();
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(user);
+  const onSubmit = (data: Form) => {
+    console.log(data);
   };
+
   return (
     // emet
     // form>div.mb-3*2>label.form-label+input#name.form-control
     // button.btn.btn-primary --- emet
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Nombre
         </label>
         <input
-          value={user.name}
-          onChange={(e) => setUser({ ...user, name: e.target.value })}
+          {...register("name", {
+            minLength: {
+              value: 3,
+              message: "El nombre debe tener al menos 3 caracteres",
+            },
+          })}
           type="text"
           id="name"
           className="form-control"
         />
+        {errors.name && <p>{errors?.name?.message}</p>}
       </div>
       <div className="mb-3">
         <label htmlFor="lastname" className="form-label">
           Apellido
         </label>
         <input
-          value={user.lastname}
-          onChange={(e) => setUser({ ...user, lastname: e.target.value })}
+          {...register("lastname")}
           type="text"
           id="lastname"
           className="form-control"
