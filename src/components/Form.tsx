aac3715d0955ce55e58a49cbf722c1ba193ab9c0
uescobar <uescobar.360@gmail.com>
2026-02-11 13:40:55 -0600

    Formulario Uncontrolled Components

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,18 @@
-import React, { type FormEvent } from "react";
+import React, { useRef, type FormEvent } from "react";
 
 type Props = {};
 
 function Form({}: Props) {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const lastnameRef = useRef<HTMLInputElement>(null);
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Enviando...");
+    const user = {
+      name: nameRef.current?.value,
+      lastname: lastnameRef.current?.value,
+    };
+    console.log(user);
   };
   return (
     // emet
@@ -16,13 +23,18 @@ function Form({}: Props) {
         <label htmlFor="name" className="form-label">
           Nombre
         </label>
-        <input type="text" id="name" className="form-control" />
+        <input ref={nameRef} type="text" id="name" className="form-control" />
       </div>
       <div className="mb-3">
         <label htmlFor="lastname" className="form-label">
           Apellido
         </label>
-        <input type="text" id="lastname" className="form-control" />
+        <input
+          ref={lastnameRef}
+          type="text"
+          id="lastname"
+          className="form-control"
+        />
       </div>
 
       <button className="btn btn-primary">Enviar</button>
